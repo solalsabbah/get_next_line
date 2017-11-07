27bb0d53cb91040c47889482dcaf74b747d856cf
Solal SABBAH <ssabbah@e2r1p1.42.fr>
2017-11-07 12:28:16 +0100

    final work with norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,26 @@
 /*   By: ssabbah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 17:26:26 by ssabbah           #+#    #+#             */
-/*   Updated: 2017/11/02 19:04:26 by ssabbah          ###   ########.fr       */
+<<<<<<< Updated upstream
+/*   Updated: 2017/11/07 12:17:23 by ssabbah          ###   ########.fr       */
+=======
+/*   Updated: 2017/11/07 10:22:44 by ssabbah          ###   ########.fr       */
+>>>>>>> Stashed changes
 /*                                                                            */
 /* ************************************************************************** */
 
-/// pk le buff ne repart pas du bébut du fichier ?
-/// tmp et buf s'écrase à certain moment 
-
 #include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "libft/libft.h"
 
 #define BUFF_SIZE 10
 
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
-
+<<<<<<< Updated upstream
 int		ft_remaintoline(char **rem, char **line)
 {
-	int	len; 
-	
+	int	len;
+
 	if (ft_strchr(*rem, '\n') != NULL)
 	{
 		len = ft_strchr(*rem, '\n') - *rem;
@@ -40,90 +33,73 @@ int		ft_remaintoline(char **rem, char **line)
 		*rem = ft_strchr(*rem, '\n') + 1;
 		return (1);
 	}
-	line [0] = ft_strdup(*rem);
+	line[0] = ft_strdup(*rem);
 	*rem = NULL;
 	return (0);
 }
 
 int		ft_readfunction(int fd, char **rem, char **line, char *buf)
 {
-	int 	ret;
-	int	len; 
+	int		ret;
+	int		len;
 	char	*tmp;
-		
+
 	while ((ret = read(fd, buf, BUFF_SIZE)) != 0)
 	{
 		if (ret == -1)
 			return (-1);
 		buf[ret] = 0;
-		printf("buf >> %s << \n", buf);
 		tmp = ft_strdup(line[0]);
 		free(line[0]);
-		line[0] = ft_strjoin(tmp, buf); // pb join tmp et buf
-		printf("tmp : %s\n", tmp); // understanding tmp & buf
-		//printf(GREEN ">> %s <<\n" RESET, line[0]); //// print line
+		line[0] = ft_strjoin(tmp, buf);
 		free(tmp);
 		if (ft_strchr(line[0], '\n') != NULL)
 		{
 			len = ft_strchr(line[0], '\n') - line[0];
 			*rem = ft_strdup(ft_strchr(line[0], '\n') + 1);
-			printf(BLUE "je rentre ici\n"RESET);
-			//printf("rem 1 %s\n", *rem); /// print remaining 
 			tmp = ft_strdup(line[0]);
 			free(line[0]);
 			line[0] = ft_strsub(tmp, 0, len);
-			//printf(BLUE ">> tmp %s\n <<" RESET, tmp); // print tmp
 			free(tmp);
-			break ; 
+			break ;
 		}
+=======
+int		get_next_line(const int fd, char **line)
+{
+	int				ret;
+	static char		*rem;
+	char			*str;
+
+	line[0] = ft_strnew(BUF_SIZE);
+	str = ft_strnew(BUF_SIZE);
+	while ((ret = read(fd, line, BUF_SIZE)) != 0)
+	{
+		printf("%s\n", line[0]);
+		line[ret] = 0;
+>>>>>>> Stashed changes
 	}
 	return (ret);
-
 }
-
 
 int		get_next_line(const int fd, char **line)
 {
 	static char	*rem = NULL;
 	char		*buf;
-	int		ret;
-	
+	int			ret;
+
 	if (line == NULL || fd < 0 || BUFF_SIZE < 1)
 		return (-1);
 	line[0] = ft_strnew(BUFF_SIZE);
 	if (rem != NULL)
 		if ((ft_remaintoline(&rem, &line[0])) != 0)
 		{
-			//printf("line" CYAN ">> %s<<\n" RESET, line[0]); // print final line
 			return (1);
 		}
-	//printf(">> rem %s <<\n", rem); // print remaing 
 	buf = ft_strnew(BUFF_SIZE);
 	if ((ret = ft_readfunction(fd, &rem, &line[0], buf)) == -1)
 		return (-1);
-	//printf(RED "%s\n" RESET, line[0]); // print final line
 	free(buf);
 	if (ret == 0 && ft_strlen(line[0]) == 0)
 		return (0);
-	//printf("line" GREEN ">> %s<<\n" RESET, line[0]);
 	return (1);
-}
-
-int		main(int argc, char **argv)
-{
-	int	fd;
-	char	*line;
-	
-	fd = open(argv[1], O_RDONLY);
-	printf("gnl : %d\n", get_next_line(fd, &line));
-	printf("-----------------\n");
-	printf("gnl : %d\n", get_next_line(fd, &line));
-	printf("-----------------\n");
-	printf("gnl : %d\n", get_next_line(fd, &line));
-	printf("-----------------\n");
-	printf("gnl : %d\n", get_next_line(fd, &line));
-	printf("-----------------\n");
-	printf("gnl : %d\n", get_next_line(fd, &line));
-
-		return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,9 +6,14 @@
 /*   By: ssabbah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 17:26:16 by ssabbah           #+#    #+#             */
-/*   Updated: 2017/10/12 17:47:45 by ssabbah          ###   ########.fr       */
+/*   Updated: 2017/11/07 12:12:51 by ssabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
+
+#endif
